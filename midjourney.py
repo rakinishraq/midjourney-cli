9ab85c7c5f50a58031c0c86597f957d07017c978
name <email>
2024-06-05 14:20:13 -0400

    completed basic imagine/seed

--- midjourney.py
+++ midjourney.py
@@ -2,21 +2,13 @@ import config
 import requests
 from time import sleep
 import webbrowser
+import json
+import os
  
-verbose = print 
-
-def is_valid_url(url):
-    if not url: return False
-    try:
-        response = requests.get(url)
-        return response.status_code == 200
-    except requests.exceptions.RequestException as e:
-        if verbose: print(f"Invalid URL: {url}. Error: {e}")
-        return False
-
-
+def verbose(*a): return a 
 headers = {"X-API-KEY": config.API_KEY}
-# ADD WEBHOOK AND BOT_ID HANDLING HERE
+
+
 def goapi(endpoint, **kwargs):
     url = "https://api.midjourneyapi.xyz/mj/v2/"+endpoint
     verbose(url, kwargs)
@@ -41,6 +33,7 @@ def imagine(prompt, aspect_ratio="1:1", process_mode="fast",
     bot_id (str): Force task processing on specific Discord account
     """
 
+    # Prompt
     if sref:
         sref_strings = ['--sref']
         for r in sref:
@@ -49,21 +42,63 @@ def imagine(prompt, aspect_ratio="1:1", process_mode="fast",
             else:
                 sref_strings.append(r)
         if sw: sref_strings.extend(('--sw', str(sw)))
-        prompt += ' ' + ' '.join(sref_strings)
-    
+        prompt += ' ' + ' '.join(sref_strings)    
     if cref:
         prompt += ' --cref ' + ' '.join(cref)
         if cw: prompt += ' --cw ' + cw
 
+    # Imagine
     out = goapi("imagine", prompt=prompt,
                 aspect_ratio=aspect_ratio,
                 process_mode=process_mode)
-    id = out["task_id"]
-    if not id: return out
+    if not (id := out["task_id"]):
+        raise Exception(out)
+    status = "started"
+    print("imagining:", url := "https://img.midjourneyapi.xyz/mj/%s.png" % id)
 
+    while status != "finished":
+        out = goapi("fetch", task_id=id)
+        status = out["status"]
+
+        if status == "failed": raise Exception(out)
+        elif status == "finished":
+            print("imagined")
+            try:
+                response = requests.get(url)
+                with open(f'output/{id}.png', 'wb') as f:
+                    f.write(response.content)
+                    os.system("feh output/%s.png" % id)
+            except Exception as e:
+                webbrowser.open(url)
+                print("Downloading image failed, opening in browser.")
+                print(e)
+            with open(f'output/{id}.json', 'w') as f:
+                json.dump(out, f, indent=4)        
+            break
+
+        sleep(1)
+    
+    # Seed
+    seed_id = goapi("seed", task_id=id)["task_id"]
+    status = "pending"
+
+    while status in ["pending", "processing"]:
+        out = goapi("fetch", task_id=seed_id)
+        status = out["status"]
+
+        if status == "failed": raise Exception(out)
+        elif status == "finished":
+            with open(f'output/{id}.json', 'w') as f:
+                json.dump(out, f, indent=4)
+            print("seeded:", out["task_result"]["seed"])
+            break
+
+        sleep(1)
 
 def main():
-    imagine("test")
+    if not os.path.exists("output"):
+        os.makedirs("output")
+    imagine("artistic, business card, strong typography, eye-catching, minimal, QR code, silver foil")
 
 
 if __name__ == "__main__":
